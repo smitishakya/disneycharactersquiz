820e2f99afd379a3a8fa3857cb87bcb2c622c67c
Smiti Shakya <smitishk12@gmail.com>
2020-03-04 11:48:15 +0545

    added disney character pictures

--- main.js
+++ main.js
@@ -1,57 +1,107 @@
 let score = 0;
 let currentQuestion = 0;
+
 let questions = [
-    {
-      title: "What color is Squirtle?",
-      answers: ['yellow', 'blue', 'orange', 'purple'],
-      correct: 1
-    },
-    {
-      title: "What color is Bulbasuar?",
-      answers: ['yellow', 'blue', 'orange', 'purple'],
-      correct: 3
-    },
-    {
-      title: "What color is Pikachu?",
-      answers: ['yellow', 'blue', 'orange', 'purple'],
-      correct: 0
-    },
-    {
-      title: "What color is Charmander?",
-      answers: ['yellow', 'blue', 'orange', 'purple'],
-      correct: 2
+  {
+    title: "Who is Jasmine?",
+    answers: ['yellow', 'blue', 'orange', 'purple'],
+    correct: 1
+  },
+  {
+    title: "Who is Cinderella?",
+    answers: ['yellow', 'blue', 'orange', 'purple'],
+    correct: 3
+  },
+  {
+    title: "Who is Mermaid?",
+    answers: ['yellow', 'blue', 'orange', 'purple'],
+    correct: 0
+  },
+  {
+    title: "Who is Snowwhite?",
+    answers: ['yellow', 'blue', 'orange', 'purple'],
+    correct: 2
+  }
+];
+
+$(document).ready(function(){
+  $('.start a').click(function(e){
+    e.preventDefault();
+    $('.start').hide();
+    $('.quiz').show();
+    showQuestion();
+    
+  });  
+  $('.quiz ul').on('click', 'li', function(){
+    $('.selected').removeClass('selected');
+    $(this).toggleClass('selected');
+  })
+  
+  $('.quiz a').click(function(e){
+    e.preventDefault();
+    if($('li.selected').length){
+      let guess = parseInt($('li.selected').attr('id'));
+      checkAnswer(guess);
     }
-  ];
-
-  $(document).ready(function(){
-
-    $('.start a').click(function(e){
-        e.preventDefault();
-        $('.start').hide();
-        $('.quiz').show();
-        showQuestion();
-    });
-    $('.quiz ul').on('click', 'li', function(){
-        $('.selected').removeClass('selected');
-        $(this).toggleClass('selected');
-    });
-
+    else{
+      alert('Please select an answer');
+    }
+    
   });
+  $('.summary a').click(function(e){
+    e.preventDefault();
+    restartQuiz();
+   })
+ 
+});
 
-  function startQuiz(){
 
+function showQuestion(){
+  let question = questions[currentQuestion];
+  $('.quiz h2').text(question.title);
+  //clearing any text from html 
+  $('.answers').html('');
+  for (var i=0; i<question.answers.length; i++){
+    $('.answers').append('<li id="'+i+'" class="answer '+question.answers[i]+'">'+question.answers[i]+'</li>')
   }
+  
+}
 
-  function showQuestion(){
-      let question = questions[currentQuestion];
-      $('.quiz h2').text(question.title);
-      $('.quiz ul').html('');
-      for(var i=0; i<question.answers.length; i++){
-          $('.quiz ul').append(`<li id="${i}">${question.answers[i]}</li>`);
-      }
-
+function checkAnswer(guess){
+  let question = questions[currentQuestion];
+  if(question.correct === guess){
+    score++;  
+    
   }
-
-  function checkAnswer(){
-
+  currentQuestion++;
+  if(currentQuestion >= questions.length){
+    showSummary();
   }
+  else{
+    showQuestion();
+  }
+  
+}
+
+function showSummary(){
+  $('.quiz').hide();
+  $('.summary').show();
+  $('.summary p').text("Congrats you scored "+ score + " out of "+ questions.length+" correct!");
+  $('img.show').removeClass('show');
+  if(score >= 3){
+    $('.happy-ditto').addClass('show');
+  }
+  else{
+    $('.sad-ditto').addClass('show');
+  }
+  
+}
+
+function restartQuiz(){
+    $('.summary').hide();
+    $('.quiz').show();
+    score = 0;
+    currentQuestion = 0;
+    showQuestion();
+ 
+}
